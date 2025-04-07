4d0547cf086f4c2b94f833c44a25b0518bc45019
한서정 / Seojung Han <sjhan0814@gmail.com>
2025-04-07 23:15:35 +0900

    feat. 대본유사도

--- SPitching/src/components/feedback/summary/SimilarityCard.tsx
+++ SPitching/src/components/feedback/summary/SimilarityCard.tsx
@@ -1,6 +1,21 @@
+import TriangleIcon from '../../../assets/triangle.svg?react';
+
 const SimilarityCard = () => {
   return (
-    <div className='white-card col-span-3 col-start-2 row-span-2 row-start-7'></div>
+    <article className='white-card col-span-3 col-start-2 row-span-2 row-start-7 justify-center gap-3'>
+      <p className='s1 text-gray-900'>대본 유사도</p>
+      <div className='flex shrink-0 items-center justify-between self-stretch'>
+        <div className='flex flex-col items-start gap-1'>
+          <span className='b2 text-gray-900'>높은 유사도입니다</span>
+          <div className='c2 flex items-center text-gray-700'>
+            <TriangleIcon className='mr-1 text-rose-500' />
+            <span>유사도가&nbsp;</span>
+            <span className='c1 text-rose-500'>5%&nbsp;</span>
+            <span>상승했습니다.</span>
+          </div>
+        </div>
+      </div>
+    </article>
   );
 };
 
