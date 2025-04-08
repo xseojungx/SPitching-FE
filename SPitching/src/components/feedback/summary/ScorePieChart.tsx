cb40e147446c57afe5951bf1adadd53b4e8e9265
한서정 / Seojung Han <sjhan0814@gmail.com>
2025-04-08 11:26:02 +0900

    feat. pie chart

---
+++ SPitching/src/components/feedback/summary/ScorePieChart.tsx
@@ -0,0 +1,70 @@
+import {
+  RadialBarChart,
+  RadialBar,
+  ResponsiveContainer,
+  PolarAngleAxis,
+} from 'recharts';
+
+interface ScorePieChartProps {
+  value: number;
+}
+
+const ScorePieChart = ({ value }: ScorePieChartProps) => {
+  const data = [{ name: 'progress', uv: value, fill: 'url(#gradient)' }];
+  return (
+    <ResponsiveContainer
+      width='100%'
+      height='100%'
+    >
+      <RadialBarChart
+        cx='50%'
+        cy='50%'
+        width={72}
+        height={72}
+        innerRadius='70%'
+        outerRadius='100%'
+        barSize={20}
+        data={data}
+        startAngle={450}
+        endAngle={90}
+      >
+        {/* 백그라운드 영역용 가짜 축 */}
+        <PolarAngleAxis
+          type='number'
+          domain={[0, 100]}
+          angleAxisId={0}
+          tick={false}
+          width={72}
+          height={72}
+        />
+        <RadialBar
+          background
+          dataKey='uv'
+          cornerRadius={10}
+          width={72}
+          height={72}
+        />
+        <defs>
+          <linearGradient
+            id='gradient'
+            x1='0.5'
+            y1='0'
+            x2='0.5'
+            y2='1'
+          >
+            <stop
+              offset='0%'
+              stopColor='#A9EAD6'
+            />
+            <stop
+              offset='100%'
+              stopColor='#255A9B'
+            />
+          </linearGradient>
+        </defs>
+      </RadialBarChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default ScorePieChart;
--- SPitching/src/components/feedback/summary/SimilarityCard.tsx
+++ SPitching/src/components/feedback/summary/SimilarityCard.tsx
@@ -1,17 +1,26 @@
 import TriangleIcon from '../../../assets/triangle.svg?react';
-
+import ScorePieChart from './ScorePieChart';
 const SimilarityCard = () => {
   return (
-    <article className='white-card col-span-3 col-start-2 row-span-2 row-start-7 justify-center gap-3'>
-      <p className='s1 text-gray-900'>대본 유사도</p>
-      <div className='flex shrink-0 items-center justify-between self-stretch'>
+    <article className='white-card col-span-3 col-start-2 row-span-2 row-start-7'>
+      <p className='s1 justify-self-start text-gray-900'>대본 유사도</p>
+
+      <div className='flex flex-1 shrink-0 items-center justify-between self-stretch'>
         <div className='flex flex-col items-start gap-1'>
           <span className='b2 text-gray-900'>높은 유사도입니다</span>
-          <div className='c2 flex items-center text-gray-700'>
+          <div className='c2 flex items-center whitespace-nowrap text-gray-700'>
             <TriangleIcon className='mr-1 text-rose-500' />
             <span>유사도가&nbsp;</span>
             <span className='c1 text-rose-500'>5%&nbsp;</span>
             <span>상승했습니다.</span>
+          </div>
+        </div>
+
+        {/* 차트 */}
+        <div className='relative h-20 w-20'>
+          <ScorePieChart value={80} />
+          <div className='h2 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-gray-900'>
+            80
           </div>
         </div>
       </div>
