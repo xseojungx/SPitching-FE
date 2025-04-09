1bc1534fab52141aa7cddfdc9336bd24966df501
한서정 / Seojung Han <sjhan0814@gmail.com>
2025-04-09 11:45:28 +0900

    style. 글자 색상 수정

--- SPitching/src/components/feedback/summary/EyeContactCard.tsx
+++ SPitching/src/components/feedback/summary/EyeContactCard.tsx
@@ -1,6 +1,8 @@
 const EyeContactCard = () => {
   return (
-    <div className='white-card col-span-3 col-start-9 row-span-2 row-start-9'></div>
+    <div className='white-card col-span-3 col-start-9 row-span-2 row-start-9'>
+      <p className='s1 justify-self-start text-gray-900'>시선</p>
+    </div>
   );
 };
 
--- SPitching/src/components/feedback/summary/GestureScoreCard.tsx
+++ SPitching/src/components/feedback/summary/GestureScoreCard.tsx
@@ -2,8 +2,11 @@ const GestureScoreCard = () => {
   return (
     <article className='white-card col-span-3 col-start-9 row-span-4 row-start-5'>
       <p className='s1 justify-self-start text-gray-900'>제스처</p>
-      <div className='flex w-full justify-center'>
-        <p className='h1'>89</p>
+      <div className='flex w-full flex-col items-center justify-center'>
+        <p className='b2 text-gray-700'>총점</p>
+        <p className='h1 bg-gradient-to-b from-[#255A9B] via-[#7AB7CE] to-[#A9EAD6] bg-clip-text text-transparent'>
+          89
+        </p>
       </div>
     </article>
   );
