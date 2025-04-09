72478ee4812e99ca8981ec74e9afad5561ef11e8
한서정 / Seojung Han <sjhan0814@gmail.com>
2025-04-09 10:53:11 +0900

    feat. gesture title

--- SPitching/src/components/feedback/summary/GestureScoreCard.tsx
+++ SPitching/src/components/feedback/summary/GestureScoreCard.tsx
@@ -1,6 +1,11 @@
 const GestureScoreCard = () => {
   return (
-    <div className='white-card col-span-3 col-start-9 row-span-4 row-start-5'></div>
+    <article className='white-card col-span-3 col-start-9 row-span-4 row-start-5'>
+      <p className='s1 justify-self-start text-gray-900'>제스처</p>
+      <div className='flex w-full justify-center'>
+        <p className='h1'>89</p>
+      </div>
+    </article>
   );
 };
 
