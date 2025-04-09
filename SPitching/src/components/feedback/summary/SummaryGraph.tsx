92f28a9bae5f46855a98573d43879468c98ffbec
한서정 / Seojung Han <sjhan0814@gmail.com>
2025-04-09 13:44:25 +0900

    feat. summary area chart, legend over effect

--- SPitching/src/components/feedback/summary/SummaryGraph.tsx
+++ SPitching/src/components/feedback/summary/SummaryGraph.tsx
@@ -1,7 +1,9 @@
+import SummaryAreaChart from './SummaryAreaChart';
+
 const SummaryGraph = () => {
   return (
     <article className='white-card col-span-7 col-start-2 row-span-5 row-start-2 w-full'>
-      <span>asdf</span>
+      <SummaryAreaChart />
     </article>
   );
 };
