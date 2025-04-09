4f3ab0a57c22975e3d913dd4126390f7c8ee0cd3
한서정 / Seojung Han <sjhan0814@gmail.com>
2025-04-09 16:04:46 +0900

    feat. gesture score

--- SPitching/src/components/feedback/summary/EyeContactCard.tsx
+++ SPitching/src/components/feedback/summary/EyeContactCard.tsx
@@ -1,6 +1,6 @@
 const EyeContactCard = () => {
   return (
-    <div className='white-card col-span-3 col-start-9 row-span-2 row-start-9'>
+    <div className='white-card col-span-3 col-start-9 row-span-3 row-start-8'>
       <p className='s1 justify-self-start text-gray-900'>시선</p>
     </div>
   );
