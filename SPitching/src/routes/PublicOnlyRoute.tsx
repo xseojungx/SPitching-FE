0c82ff50091609b2c670f8d72b100c073e9a3cd8
한서정 / Seojung Han <sjhan0814@gmail.com>
2025-04-13 05:00:38 +0900

    fix.JSX 오류

--- SPitching/src/routes/PublicOnlyRoute.tsx
+++ SPitching/src/routes/PublicOnlyRoute.tsx
@@ -3,7 +3,7 @@ import { Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { RootState } from '@/redux/store';
 
-const PublicOnlyRoute = ({ children }: { children: JSX.Element }) => {
+const PublicOnlyRoute = ({ children }: { children: React.JSX.Element }) => {
   const isLoggedIn = useSelector((state: RootState) => state.auth.isLoggedIn);
 
   return isLoggedIn ? (
