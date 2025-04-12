f30354c789bf4e5fa7b108a25a238575f553d417
한서정 / Seojung Han <sjhan0814@gmail.com>
2025-04-12 15:53:17 +0900

    feat. 최근 연습 임시

--- SPitching/src/pages/dashboard/Dashboard.tsx
+++ SPitching/src/pages/dashboard/Dashboard.tsx
@@ -15,7 +15,14 @@ const Dashboard = () => {
       <Navbar />
       {/* 모바일에선 여백 없이 꽉 채움 */}
       <div className='relative box-border flex min-h-2/3 w-screen items-center pt-18'>
-        <RecentPractice />
+        {data?.data[0] && (
+          <RecentPractice
+            title={data.data[0].title}
+            description={data.data[0].description}
+            practice_count={data.data[0].practice_count}
+          />
+        )}
+
         <div className="absolute left-0 -z-1 h-full w-screen bg-[url('/assets/dashboard_bg.svg')] bg-left-top bg-no-repeat not-first:bg-cover"></div>
       </div>
       <div className='box-border flex w-8/12 flex-col items-center gap-2 pt-18'>
