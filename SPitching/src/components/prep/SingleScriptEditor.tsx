5563179ca16e8d9d4cd657fbe85241ee76c390a0
한서정 / Seojung Han <sjhan0814@gmail.com>
2025-05-05 13:43:48 +0900

    feat. tag ui 제작

--- SPitching/src/components/prep/SingleScriptEditor.tsx
+++ SPitching/src/components/prep/SingleScriptEditor.tsx
@@ -1,5 +1,6 @@
 import { useRef, useState, useEffect } from 'react';
 import PPTImage from '@/assets/mock_ppt_1.png';
+import TagInput from './TagInput';
 
 const ScriptEditor = () => {
   const [text, setText] = useState('');
@@ -15,7 +16,7 @@ const ScriptEditor = () => {
   return (
     <div className='flex h-auto w-full justify-between overflow-x-hidden py-10'>
       {/* 피피티 사진 */}
-      <div className='flex w-[20%] flex-col justify-center self-start px-10 pt-3'>
+      <div className='flex h-fit w-[20%] flex-col justify-center self-start px-10 pt-3'>
         <img
           className='shadow-[rgba(64, 80, 98, 0.1)] h-fit w-full rounded-[4px] object-contain shadow-xl'
           src={PPTImage}
@@ -27,15 +28,26 @@ const ScriptEditor = () => {
       </div>
 
       {/* 텍스트 편집 부분 */}
-      <div className='flex w-8/10 pr-10 pl-4'>
-        <textarea
-          ref={textareaRef}
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-          placeholder='발표 대본을 입력하세요'
-          className='b1 focus:outline-navy-700 focus:ring-navy-700 min-h-full w-full resize-none overflow-hidden rounded-l border-0 px-4 py-3 text-base leading-relaxed text-gray-700 outline-0 focus:text-gray-900 focus:ring-1'
-          rows={1}
-        />
+      <div className='flex min-h-full w-8/10 flex-col pr-10 pl-4'>
+        <div className='flex-1'>
+          <textarea
+            ref={textareaRef}
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder='발표 대본을 입력하세요'
+            className='b1 focus:outline-navy-700 focus:ring-navy-700 min-h-full w-full overflow-hidden rounded-[4px] border-0 px-4 py-3 text-base leading-relaxed text-gray-700 outline-0 focus:text-gray-900 focus:ring-1'
+            rows={1}
+          />
+        </div>
+        {/* 태그 편집 부분 */}
+        <div className='mt-2 flex h-fit w-full items-start'>
+          <TagInput />
+          <div className='ml-4 flex flex-wrap space-y-1 space-x-2'>
+            <div className='from-coral-400 b1 flex h-8 w-fit items-center rounded-full bg-gradient-to-br to-rose-500 px-8 text-white'>
+              태그
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
