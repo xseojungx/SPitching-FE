b6ce31130594abd92bb323db73c27a52013a8e0a
한서정 / Seojung Han <sjhan0814@gmail.com>
2025-05-18 13:11:11 +0900

    fix: 로그인 경로 수정

--- SPitching/src/components/common/Navbar.tsx
+++ SPitching/src/components/common/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   const navigate = useNavigate();
 
   const handleGoogleLogin = () => {
-    window.location.href = import.meta.env.VITE_API_BASE_URL + 'oauth2/authorization/google';
+    window.location.href = import.meta.env.VITE_API_BASE_URL + '/oauth2/authorization/google';
   };
 
   return (
