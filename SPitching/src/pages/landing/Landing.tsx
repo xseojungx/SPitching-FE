eced7616955b43b313aa3396e75a18e7f7ea45ac
한서정 / Seojung Han <sjhan0814@gmail.com>
2025-06-03 14:43:25 +0900

    feat: landing 사진 추가

--- SPitching/src/pages/landing/Landing.tsx
+++ SPitching/src/pages/landing/Landing.tsx
@@ -3,8 +3,9 @@ import Navbar from '@/components/common/Navbar';
 import { useSelector } from 'react-redux';
 import { RootState } from '@/redux/store';
 import { useNavigate } from 'react-router-dom';
-import Gesture from '@/assets/gesture_landing.png';
-import Eyecontact from '@/assets/eyecontact_landing.png';
+import Gesture from '@/assets/gesture_landing.jpg';
+import Eye from '@/assets/eye_landing.jpg';
+import Fluency from '@/assets/flu.jpg';
 export default function Landing() {
   const handleGoogleLogin = () => {
     window.location.href = import.meta.env.VITE_API_BASE_URL + '/oauth2/authorization/google';
@@ -41,34 +42,34 @@ export default function Landing() {
       </main>
 
       {/* Features Section */}
-      <section className='bg-neutral-50/70 py-16'>
+      <section className='bg-white/70 py-16'>
         <div className='mx-auto grid max-w-5xl grid-cols-1 gap-8 px-6 md:grid-cols-3'>
           <div className='flex flex-col items-center'>
-            {/* <img
-              src={Eyecontact}
+            <img
+              src={Eye}
               alt='시선 추적'
-              className='mb-4 h-40'
-            /> */}
+              className='mb-4 h-50 rounded-2xl'
+            />
             <h3 className='mb-2 text-xl font-semibold'>시선 추적 피드백</h3>
             <p className='text-center text-base'>발표 중 시선 분포 분석으로 시청자 집중 유도</p>
           </div>
 
           <div className='flex flex-col items-center'>
-            {/* <img
+            <img
               src={Gesture}
               alt='제스처 인식'
-              className='mb-4 h-16'
-            /> */}
+              className='mb-4 h-50 rounded-2xl'
+            />
             <h3 className='mb-2 text-xl font-semibold'>제스처 인식 피드백</h3>
             <p className='text-center text-base'>핸드 제스처 분석으로 자연스러운 동작 완성</p>
           </div>
 
           <div className='flex flex-col items-center'>
-            {/* <img
-              src='/images/fluency.svg'
+            <img
+              src={Fluency}
               alt='음성 유창성'
-              className='mb-4 h-16'
-            /> */}
+              className='mb-4 h-50 rounded-2xl'
+            />
             <h3 className='mb-2 text-xl font-semibold'>유창성 분석</h3>
             <p className='text-center text-base'>발음과 속도 검토로 매끄러운 전달</p>
           </div>
